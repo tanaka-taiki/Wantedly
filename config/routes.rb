b2410a6a5bd1f36e95dbbab750e3d909abad0fc0
natsumican0725 <natsumican725.summer@gmail.com>
2018-10-30 13:10:24 +0900

    Create-tools-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'projects#index'
+  root 'tools#index'
   resources :users, only: [:index, :edit, :update, :destroy]
+  resources :introductions, only: [:index]
+  resources :projects, only: [:index]
+  resources :abouts, only: [:index]
+  resources :feeds, only: [:index]
+  resources :tools, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
