169aa010d6c018431ac88a88928a41e2f5eef4de
natsumican0725 <natsumican725.summer@gmail.com>
2018-10-31 22:07:47 +0900

    Add-main-footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tools#index'
+  root 'projects#index'
   resources :users, only: [:index, :edit, :update, :destroy]
   resources :introductions, only: [:index]
   resources :projects, only: [:index]
