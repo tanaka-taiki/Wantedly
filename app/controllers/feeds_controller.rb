2beebd437b4eafed4e8ccc5739218d90f2992344
natsumican0725 <natsumican725.summer@gmail.com>
2018-11-01 16:15:44 +0900

    Add-home

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,7 @@
 class FeedsController < ApplicationController
   def index
   end
+
+  def work_style
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'projects#index'
+  root 'homes#index'
   resources :users, only: [:index, :edit, :update, :destroy]
   resources :introductions, only: [:index]
   resources :projects, only: [:index]
   resources :abouts, only: [:index]
-  resources :feeds, only: [:index]
+  resources :feeds, only: [:index, :work_style]
   resources :tools, only: [:index]
+  resources :homes, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
