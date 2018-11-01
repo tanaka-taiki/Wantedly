08acbe86e6b4826e642e3d313b51682cfd9b9b31
natsumican0725 <natsumican725.summer@gmail.com>
2018-11-01 19:14:57 +0900

    Finish-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'projects#index'
+  root 'tools#index'
   resources :users, only: [:index, :edit, :update, :destroy]
   resources :introductions, only: [:index]
   resources :projects, only: [:index]
